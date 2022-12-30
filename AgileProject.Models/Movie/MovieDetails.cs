14eb9f39213476ee28b0653de3d11d270e1e8ca9
Cassandra Faith <105570288+CassFaith13@users.noreply.github.com>
2022-12-29 19:53:44 -0500

    Update MovieDetails.cs

--- AgileProject.Models/Movie/MovieDetails.cs
+++ AgileProject.Models/Movie/MovieDetails.cs
@@ -1,11 +1,10 @@
-
-
 namespace AgileProject.Models.Movie
 {
     public class MovieDetails
     {
         public int MovieId { get; set; }
         public string? Title { get; set; }
+        public string? Genre { get; set; }
         public string? Content { get; set; }
         public DateTime ReleaseDate { get; set; }
         
