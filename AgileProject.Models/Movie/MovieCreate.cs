ac321e2e9d68b167a6eaac573c58d88e5d2b0642
Cassandra Faith <105570288+CassFaith13@users.noreply.github.com>
2022-12-29 19:52:21 -0500

    Update MovieCreate.cs

--- AgileProject.Models/Movie/MovieCreate.cs
+++ AgileProject.Models/Movie/MovieCreate.cs
@@ -1,15 +1,16 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace AgileProject.Models.Movie
 {
     public class MovieCreate
     {
         [Required]
-        [MinLength(2, ErrorMessage = "{0} must be at least {1} characters long.")]
-        [MaxLength(25, ErrorMessage = "{0} must contain no more than {1} characters.")]
         public string? Title { get; set; }
         [Required]
-        [MaxLength(250, ErrorMessage = "{0} must contain no more than {1} characters.")]
-        public string? Content { get; set; }
+        public string? Genre { get; set; }
+        [Required]
+        public string? ContentRating { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
     }
 }
