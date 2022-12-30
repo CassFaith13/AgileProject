d20845be8fa02c73de9e3d36fd413cfd96878609
Celio Arias <ariascelio@gmail.com>
2022-12-29 20:55:33 -0500

    Show_Models

--- AgileProject.Models/Show/ShowCreate.cs
+++ AgileProject.Models/Show/ShowCreate.cs
@@ -2,6 +2,16 @@ namespace AgileProject.Models.Show
 {
     public class ShowCreate
     {
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Genre { get; set; }
+
+        [Required]
+        public string? ContentRating { get; set; }
         
+        [Required]
+        public DateTime ReleaseDate { get; set; }
     }
 }
--- AgileProject.Models/Show/ShowListItem.cs
+++ AgileProject.Models/Show/ShowListItem.cs
@@ -2,6 +2,11 @@ namespace AgileProject.Models.Show
 {
     public class ShowListItem
     {
-        
+        public int ShowId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Genre { get; set; }
+        public string? ContentRating { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
